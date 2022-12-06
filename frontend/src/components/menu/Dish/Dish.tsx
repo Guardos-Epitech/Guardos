3d64ab85d140d5508692b36a53413ffb605c0e71
JosefineM2000 <josefine.mende@epitech.eu>
2022-12-06 18:02:08 +0100

    [FE] [ADD] toggled extended dish view

--- frontend/src/components/menu/Dish/Dish.tsx
+++ frontend/src/components/menu/Dish/Dish.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Grid, Paper} from "@mui/material";
 
 import styles from "@src/components/menu/Dish/Dish.module.scss";
@@ -7,31 +7,33 @@ import AllergenTags from "@src/components/menu/AllergenTags/AllergenTags";
 import DishHeader from "@src/components/menu/Dish/DishHeader/DishHeader";
 
 const Dish = () => {
+    const [extended, setExtended] = useState(false);
     const dishName = "Some title";
     const dishAllergens: string[] = ["Gluten", "Lactose", "Nuts", "Lactose", "Nuts"];
     const dishDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
         "Why do we use it?\n";
+    const shortenedDescription = dishDescription.substring(0, 250);
     const price = 10.00;
 
     return (
-        <Paper className={styles.dishBox} elevation={3} sx={{ m: 2 }}>
-            <Grid container columns={3}>
+        <Paper className={styles.dishBox} elevation={3} sx={{ m: 2 }} onClick={() => setExtended(!extended)}>
+            <Grid container>
 
-                <Grid item xs={2}  className={styles.gridItemUpper}>
+                <Grid item xs={9}  className={styles.gridItemUpper}>
                     <DishHeader dishName={dishName} price={price}/>
                 </Grid>
 
-                <Grid item xs={1} className={styles.gridItemUpper}>
+                <Grid item xs={3} className={styles.gridItemUpper}>
                 </Grid>
 
-                <Grid item xs={2}  className={styles.gridItemLower}>
+                <Grid item xs={9}  className={styles.gridItemLower}>
                     <p className={styles.justificationPrint}>
-                        {dishDescription}
+                        {extended ? dishDescription : shortenedDescription}
                     </p>
                 </Grid>
 
-                <Grid item xs={1}  className={styles.gridItemLower}>
-                    <AllergenTags dishAllergens={dishAllergens}/>
+                <Grid item xs={3}  className={styles.gridItemLower}>
+                    {extended && <AllergenTags dishAllergens={dishAllergens}/>}
                 </Grid>
 
             </Grid>
