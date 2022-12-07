05ec7f7951fbdabd30ff72a64326357e5f19f5bd
azda <da@gmail.com>
2022-12-07 13:59:19 +0100

    [FE][ADD] Add resto card and media body param em -> rem

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Button from "@mui/material/Button";
 import { NavigateTo } from "../../utils/NavigateTo";
 import { useNavigate } from "react-router-dom";
 import styles from "./HomePage.module.scss";
 import Header from "@src/components/Header/Header";
 import foodbg from "@src/asset/foodbg.png";
 import InputSearch from "@src/components/InputSearch/InputSearch";
+import RestoCard from "@src/components/RestoCard/RestoCard";
 
 const HomePage = () => {
   // let navigate = useNavigate();
@@ -16,7 +16,11 @@ const HomePage = () => {
         <span className={styles.TitleSearch}>What are you looking for ?</span>
         <InputSearch />
       </div>
-      <span>Berlin - +12548 Restaurants</span>
+      <div className={styles.DivContent}>
+        <h1 className={styles.TitleCard}>Berlin - +12548 Restaurants</h1>
+        <RestoCard />
+      </div>
+      {/* filter view */}
     </div>
   );
 };
