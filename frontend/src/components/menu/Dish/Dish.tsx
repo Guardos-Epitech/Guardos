c13f15ba177c92e915ce07e6f3f49428697d7048
JosefineM2000 <josefine.mende@epitech.eu>
2022-12-08 12:07:19 +0100

    [FE] [ADD] dish element with picture

--- frontend/src/components/menu/Dish/Dish.tsx
+++ frontend/src/components/menu/Dish/Dish.tsx
@@ -5,38 +5,58 @@ import styles from "@src/components/menu/Dish/Dish.module.scss";
 
 import AllergenTags from "@src/components/menu/AllergenTags/AllergenTags";
 import DishHeader from "@src/components/menu/Dish/DishHeader/DishHeader";
+import DishWithPicture from "@src/components/menu/Dish/DishWithPicture";
 
-const Dish = () => {
+type DishProps = {
+    dishName: string,
+    dishAllergens: string[],
+    dishDescription: string,
+    imageSrc: string,
+    price: number
+}
+
+const Dish = (props: DishProps) => {
     const [extended, setExtended] = useState(false);
-    const dishName = "Some title";
-    const dishAllergens: string[] = ["Gluten", "Lactose", "Nuts", "Lactose", "Nuts"];
-    const dishDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
-        "Why do we use it?\n";
+    const {dishName, dishAllergens, dishDescription, imageSrc, price } = props;
     const shortenedDescription = dishDescription.substring(0, 250);
-    const price = 10.00;
 
     return (
         <Paper className={styles.dishBox} elevation={3} sx={{ m: 2 }} onClick={() => setExtended(!extended)}>
-            <Grid container>
+            {imageSrc ?
+                <DishWithPicture
+                    dishName={dishName}
+                    dishAllergens={dishAllergens}
+                    dishDescription={dishDescription}
+                    shortenedDescription={shortenedDescription}
+                    imageSrc={imageSrc}
+                    price={price}
+                    extended={extended}
+                /> :
+                <Grid container>
 
-                <Grid item xs={9}  className={styles.gridItemUpper}>
-                    <DishHeader dishName={dishName} price={price}/>
+                    {/*Dish name and price*/}
+                    <Grid item xs={9} className={styles.gridItemUpper}>
+                        <DishHeader dishName={dishName} price={price}/>
+                    </Grid>
+
+                    {/*Dish picture*/}
+                    <Grid item xs={3} className={styles.gridItemUpper}>
+                    </Grid>
+
+                    {/*Dish description*/}
+                    <Grid item xs={9} className={styles.gridItemLower}>
+                        <p className={styles.justificationPrint}>
+                            {extended ? dishDescription : shortenedDescription}
+                        </p>
+                    </Grid>
+
+                    {/*Dish allergens*/}
+                    <Grid item xs={3} className={styles.gridItemLower}>
+                        {extended && <AllergenTags dishAllergens={dishAllergens}/>}
+                    </Grid>
+
                 </Grid>
-
-                <Grid item xs={3} className={styles.gridItemUpper}>
-                </Grid>
-
-                <Grid item xs={9}  className={styles.gridItemLower}>
-                    <p className={styles.justificationPrint}>
-                        {extended ? dishDescription : shortenedDescription}
-                    </p>
-                </Grid>
-
-                <Grid item xs={3}  className={styles.gridItemLower}>
-                    {extended && <AllergenTags dishAllergens={dishAllergens}/>}
-                </Grid>
-
-            </Grid>
+            }
 
         </Paper>
     )
--- frontend/src/pages/MenuPage/MenuPage.tsx
+++ frontend/src/pages/MenuPage/MenuPage.tsx
@@ -7,20 +7,28 @@ import Category from "@src/components/menu/Category/Category";
 import Layout from "@src/components/layout/Layout";
 
 const MenuPage = () => {
+    const dishName = "Juicy Burger";
+    const dishAllergens: string[] = ["Gluten", "Lactose", "Nuts", "Sesame", "Tree nuts"];
+    const dishDescription = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n" +
+        "Why do we use it?\n";
+    const price = 10.00;
+    const imageSrc = "burger.jpg";
+    const pizzaImageSrc = "pizza.jpg";
+
     return (
         <Layout>
             <h1 className={styles.menuTitle}>
                 Menu Page
             </h1>
             <Category title={"Appetizers"}>
-                <Dish />
-                <Dish />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription} imageSrc={imageSrc} price={price} />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription} imageSrc={""} price={price} />
             </Category>
             <Category title={"Main Courses"}>
-                <Dish />
-                <Dish />
-                <Dish />
-                <Dish />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription + dishDescription} imageSrc={pizzaImageSrc} price={price} />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription + dishDescription} imageSrc={""} price={price} />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription} imageSrc={imageSrc} price={price} />
+                <Dish dishName={dishName} dishAllergens={dishAllergens} dishDescription={dishDescription} imageSrc={imageSrc} price={price} />
             </Category>
         </Layout>
     );
