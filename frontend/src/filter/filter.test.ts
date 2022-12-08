7bd73941257bea8808ffbfca8ecbc209462fe8d9
wernerramon <ramon.werner@epitech.eu>
2022-12-08 14:36:32 +0100

    [FE][MODIFY] added test to check if dishes are sorted right for name or group search

--- frontend/src/filter/filter.test.ts
+++ frontend/src/filter/filter.test.ts
@@ -41,10 +41,35 @@ test("filterForRestaurantwithNameorGroup", () => {
 });
 
 
-test("checkIfDishesAreFilteredRight", () => {
+test("checkIfDishesAreFilteredRightForAllergenSearch", () => {
     const filter = new Filter();
     let original = filter.restaurants;
-    const result = filter.filterForRestaurantsWithAllergens(["gluten"]);
+    const result = filter.filterForRestaurantsWithAllergens(["milk"]);
+    let check = 0;
+    let expectedRes = 0;
+    for (let org in original) {
+        for (let filtered in result) {
+            if (result[filtered].id == original[org].id) {
+                for (let num in original[org].mealtype) {
+                    if (original[org].mealtype[num].name == result[filtered].categories[num].name) {
+                        check++;
+                    }
+                }
+            }
+        }
+    }
+    for (let org in original) {
+        for (let num in original[org].mealtype) {
+            expectedRes++;
+        }
+    }
+    expect(check).toBe(expectedRes);
+});
+
+test("checkIfDishesAreSortedRightForRestaurantOrGroupSearch", () => {
+    const filter = new Filter();
+    let original = filter.restaurants;
+    const result = filter.filterForRestaurantwithNameorGroup(["ice"]);
     let check = 0;
     let expectedRes = 0;
     for (let org in original) {
