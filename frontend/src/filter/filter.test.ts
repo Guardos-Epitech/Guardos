9cde659f6d4a535ae14702fb31022130f1f2e023
wernerramon <ramon.werner@epitech.eu>
2022-12-08 13:54:58 +0100

    [FE][MODIFY] added test to check if dishes are sorted right

--- frontend/src/filter/filter.test.ts
+++ frontend/src/filter/filter.test.ts
@@ -39,3 +39,29 @@ test("filterForRestaurantwithNameorGroup", () => {
         }
     }
 });
+
+
+test("checkIfDishesAreFilteredRight", () => {
+    const filter = new Filter();
+    let original = filter.restaurants;
+    const result = filter.filterForRestaurantsWithAllergens(["gluten"]);
+    let check = 0;
+    let expectedRes = 0;
+    for (let org in original) {
+        for (let filtered in result) {
+            if (result[filtered].id == original[org].id) {
+                for (let num in original[org].mealtype) {
+                    if (original[org].mealtype[num].name == result[filtered].categories[num].name) {
+                        check++;
+                    }
+                }
+            }
+        }
+    }
+    for (let org in original) {
+        for (let num in original[org].mealtype) {
+            expectedRes++;
+        }
+    }
+    expect(check).toBe(expectedRes);
+});
