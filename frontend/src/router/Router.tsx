be2cc1ae8a3f4ae2752eae49386a8dfc278fd3c7
JosefineM2000 <josefine.mende@epitech.eu>
2022-12-09 13:44:24 +0100

    [FE] [MODIFY] scroll to page beginning on route change

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FilterPage from "@src/pages/FilterPage";
 import MenuPage from "@src/pages/MenuPage";
 import HomePage from "@src/pages/HomePage";
+import ScrollToTop from "@src/components/ScrollToTop/ScrollToTop";
 
 const MVPRouter = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="filter" element={<FilterPage />} />
         <Route path="menu" element={<MenuPage />} />
