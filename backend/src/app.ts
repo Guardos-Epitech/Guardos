0b68c39aa4c557e68e2cb328ff70ec970178b6c5
MarcPIster <marc.pister@epitech.eu>
2022-12-09 17:24:37 +0100

    [BE] [CLEAN] stick to coding-style

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,21 +1,16 @@
 import createError from 'http-errors';
-import express from "express";
-import * as path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
+import indexRouter from './routes/index';
+
+const path = require('path');
 
 function main (): void  {
     const app = express();
-    console.log("yooooooo");
-
     const port = 8081;
 
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'pug');
 
     app.use(logger('dev'));
     app.use(express.json());
@@ -24,15 +19,14 @@ function main (): void  {
     app.use(express.static(path.join(__dirname, 'public')));
 
     app.use('/', indexRouter);
-    app.use('/users', usersRouter);
 
-  // catch 404 and forward to error handler
+    // catch 404 and forward to error handler
     app.use(function(req: any, res: any, next: any) {
         next(createError(404));
     });
 
-// error handler
-    app.use(function(err: any, req: any, res: any, next: any) {
+    // error handler
+    app.use(function(err: any, req: any, res: any) {
       // set locals, only providing error in development
       res.locals.message = err.message;
       res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -49,6 +43,6 @@ function main (): void  {
 
 try {
     main()
-    } catch (err: unknown) {
-        console.error(err)
+} catch (err: unknown) {
+    console.error(err)
 }
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,9 @@
-import * as express from 'express';
+import * as express from "express";
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send('Hello World!');
+router.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
 export default router;
