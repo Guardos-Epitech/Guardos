1c0f520503409db9719199282b282b4b6b452fb9
azda <da@gmail.com>
2022-12-12 11:26:08 +0100

    [FE] [MERGE] resolve merge conflict

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -3,18 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FilterPage from "@src/pages/FilterPage";
 import MenuPage from "@src/pages/MenuPage";
 import HomePage from "@src/pages/HomePage";
-<<<<<<< HEAD
-=======
 import ScrollToTop from "@src/components/ScrollToTop/ScrollToTop";
->>>>>>> main
 
 const MVPRouter = () => {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-=======
       <ScrollToTop />
->>>>>>> main
       <Routes>
         <Route path="filter" element={<FilterPage />} />
         <Route path="menu" element={<MenuPage />} />
