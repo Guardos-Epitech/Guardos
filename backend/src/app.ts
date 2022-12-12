861a4140b810ef88ced6eab2264b898251368729
MarcPIster <marc.pister@epitech.eu>
2022-12-12 15:55:55 +0100

    [BE] [ADD] enable error handling again

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-//import createError from 'http-errors';
+import createError from 'http-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
@@ -17,20 +17,20 @@ function main (): void  {
   app.use('/api', filter);
 
   // catch 404 and forward to error handler
-  // app.use(function(req: any, res: any, next: any) { /* eslint-disable-line */
-  //   next(createError(404));
-  // });
+  app.use(function(req: any, res: any, next: any) { /* eslint-disable-line */
+    next(createError(404));
+  });
 
   // error handler
-  // app.use(function(err: any, req: any, res: any) { /* eslint-disable-line */
-  //   // set locals, only providing error in development
-  //   res.locals.message = err.message;
-  //   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  //
-  //   // render the error page
-  //   res.status(err.status || 500);
-  //   res.render('error');
-  // });
+  app.use(function(err: any, req: any, res: any) { /* eslint-disable-line */
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
   app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
