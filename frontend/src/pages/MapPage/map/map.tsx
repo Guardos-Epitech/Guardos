4bc274a41d2ea8cf000d33660971d79a14de3c65
wernerramon <ramon.werner@epitech.eu>
2022-12-16 14:08:53 +0100

    [FE] [ADD] added dynamic markers

--- frontend/src/pages/MapPage/map/map.tsx
+++ frontend/src/pages/MapPage/map/map.tsx
@@ -2,7 +2,6 @@ import React, { useRef, useState } from "react";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
 import { OSM } from "ol/source";
 import {Point} from 'ol/geom';
 import {fromLonLat, useGeographic} from 'ol/proj';
@@ -14,10 +13,17 @@ import { Feature } from "ol";
 import VectorSource from "ol/source/Vector";
 import { Vector as VL } from "ol/layer";
 import { Style, Circle as CircleStyle, Stroke, Fill } from "ol/style";
+import dummyDataRestaurant from "../../../assets/restaurantList/restaurants.json";
 
 const Berlin = [13.409523443447888, 52.52111129522459];
 const Middle = [0,0];
-const Epitech = [13.328820, 52.508540];
+const Epitech = [13.328820, 52.508540];// long,lat
+
+interface IMapLoc {
+  name: string;
+  lat: number;
+  long: number;
+}
 
 useGeographic();
 
@@ -26,10 +32,23 @@ export const MapContext = React.createContext<IMapContext | void>(undefined);
 export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
   private mapDivRef: React.RefObject<HTMLDivElement>;
   state: TMapState = {};
+  restaurants: IMapLoc[];
 
   constructor(props: TMapProps) {
     super(props);
     this.mapDivRef = React.createRef<HTMLDivElement>();
+    this.restaurants = this.getAllRestaurantsLocation()
+  }
+
+  private getAllRestaurantsLocation(): IMapLoc[] {
+    const result : IMapLoc[] = [];
+    result.pop();
+    for (const elem of dummyDataRestaurant.restaurants) {
+      const obj: IMapLoc = {name: elem.name, lat: elem.location.latitude,
+        long: elem.location.longitude};
+      result.push(obj);
+    }
+    return result;
   }
 
   componentDidMount() {
@@ -48,6 +67,17 @@ export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
         }),
       ],
     });
+
+    let markerList: Feature[] = [];
+    markerList.pop();
+
+    for (const elem of this.restaurants) {
+      const obj = new Feature({
+        type: 'geoMarker',
+        geometry: new Point([elem.long, elem.lat]),
+      });
+      markerList.push(obj);
+    }
 
     const epitechMarker = new Feature({
       type: 'geoMarker',
@@ -69,7 +99,8 @@ export class MapComponent extends React.PureComponent<TMapProps, TMapState> {
 
     const vectorLayer = new VL({
       source: new VectorSource({
-        features: [epitechMarker],
+        // features: [epitechMarker],
+        features: markerList,
       }),
       style: function (feature) {
         return styles['geoMarker'];
