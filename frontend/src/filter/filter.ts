6e73ef2f510f001f594705c3f35c08d311b96f42
Gylian Karsch <karschbuisness@web.de>
2023-04-09 09:44:31 +0200

    [BE] [ADD] added ratingCount to Restocard

--- frontend/src/filter/filter.ts
+++ frontend/src/filter/filter.ts
@@ -2,93 +2,94 @@
 // import dummyDataRestaurants from './restaurants.json';
 
 export interface IFilterObject {
-    allergenList?: string[];
-    location?: string;
-    name?: string;
-    rating?: number[]; //2 float rating lowest and highest
-    range?: number;
-    categories?: string[];
-    dishes?: any;
+  allergenList?: string[];
+  location?: string;
+  name?: string;
+  rating?: number[]; //2 float rating lowest and highest
+  range?: number;
+  categories?: string[];
+  dishes?: any;
 }
 
 export interface ICategories {
-    name: string;
-    hitRate: number;
-    dishes: [IDishFE];
+  name: string;
+  hitRate: number;
+  dishes: [IDishFE];
 }
 
 interface IMealType {
-    name: string;
-    id: number;
-    sortId: number;
+  name: string;
+  id: number;
+  sortId: number;
 }
 
 interface ICategoryBE {
-    menuGroup: string,
-    foodGroup: string,
-    extraGroup: string
+  menuGroup: string,
+  foodGroup: string,
+  extraGroup: string
 }
 
 interface ICategoryFE {
-    foodGroup: string,
-    extraGroup: string
+  foodGroup: string,
+  extraGroup: string
 }
 
 interface ILocation {
-    streetName: string,
-    streetNumber: string,
-    postalCode: string,
-    country: string;
-    city: string;
+  streetName: string,
+  streetNumber: string,
+  postalCode: string,
+  country: string;
+  city: string;
 }
 
 interface IDishBE {
-    name: string;
-    description: string;
-    price: number;
-    allergens: string;
-    category: ICategoryBE;
+  name: string;
+  description: string;
+  price: number;
+  allergens: string;
+  category: ICategoryBE;
 }
 
 export interface IDishFE {
-    name: string;
-    description: string;
-    price: number;
-    allergens: string;
-    pictures: [string];
-    category: ICategoryFE;
+  name: string;
+  description: string;
+  price: number;
+  allergens: string;
+  pictures: [string];
+  category: ICategoryFE;
 }
 
 interface IFilterObj {
-    savedFilter: IFilterObject;
-    savedRestaurants: IRestaurantFrontEnd[][];
+  savedFilter: IFilterObject;
+  savedRestaurants: IRestaurantFrontEnd[][];
 }
 
 export interface IRestaurantFrontEnd {
-    name: string;
-    id: number;
-    phoneNumber: string;
-    categories: [ICategories];
-    location: ILocation;
-    pictures: [string];
-    rating: number;
-    range: number
-    description: string;
-    hitRate: number;
-    dishes: any;
+  name: string;
+  id: number;
+  phoneNumber: string;
+  categories: [ICategories];
+  location: ILocation;
+  pictures: [string];
+  rating: number;
+  ratingCount: number;
+  range: number
+  description: string;
+  hitRate: number;
+  dishes: any;
 }
 
 export interface IRestaurantBackEnd {
-    name: string;
-    id: number;
-    phoneNumber: string;
-    description: string;
-    rating: number;
-    range: number;
-    dishes: [IDishBE];
-    location: ILocation;
-    mealType: [IMealType];
-    extra: [IDishBE];
+  name: string;
+  id: number;
+  phoneNumber: string;
+  description: string;
+  rating: number;
+  range: number;
+  dishes: [IDishBE];
+  location: ILocation;
+  mealType: [IMealType];
+  extra: [IDishBE];
 }
 
 // class FilterQuery {
